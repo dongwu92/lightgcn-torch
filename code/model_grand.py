003dd4cd5353f74600c61c8d7bd70f24b33d66c3
mad-domng <826074356@qq.com>
2020-11-23 17:57:51 +0800

    agg weight

--- code/model_grand.py
+++ code/model_grand.py
@@ -89,10 +89,6 @@ class LightGCN(BasicModel):
         self.config = config
         self.dataset : dataloader.BasicDataset = dataset
         self.__init_weight()
-        self.sample = 4
-        self.dropnode_rate = 0.5
-        self.tem = 0.5
-        self.lam = 1.
 
     def __init_weight(self):
         self.num_users  = self.dataset.n_users
@@ -119,6 +115,7 @@ class LightGCN(BasicModel):
             print('use pretarined data')
         self.f = nn.Sigmoid()
         self.Graph = self.dataset.getSparseGraph()
+        self.agg_weight = torch.nn.Parameter(torch.FloatTensor(self.latent_dim, self.latent_dim))
         print(f"lgn is already to go(dropout:{self.config['dropout']})")
 
         # print("save_txt")
@@ -141,114 +138,77 @@ class LightGCN(BasicModel):
         else:
             graph = self.__dropout_x(self.Graph, keep_prob)
         return graph
-
-
-
-
-    def propagate(self, feature):
-        #feature = F.dropout(feature, args.dropout, training=training)
-        x = feature
-        y = feature
-        for i in range(self.n_layers):
-            x = torch.spmm(self.Graph, x).detach_()
-            y.add_(x)
-        return y.div_(self.n_layers+1.0).detach_()
-
-    def rand_prop(self, features, training):
-        n = features.shape[0]
-        drop_rate = self.dropnode_rate
-        drop_rates = torch.FloatTensor(np.ones(n) * drop_rate)
-        if training:
-            masks = torch.bernoulli(1. - drop_rates).unsqueeze(1)
-            features = masks.cuda() * features
-        else:
-            features = features * (1. - drop_rate)
-        features = self.propagate(features)
-        return features
-        
-    def consis_loss(self, logps):
-        ps = [torch.exp(p) for p in logps]
-        sum_p = 0.
-        for p in ps:
-            sum_p = sum_p + p
-        avg_p = sum_p/len(ps)
-        #p2 = torch.exp(logp2)
-        
-        sharp_p = (torch.pow(avg_p, 1./self.tem) / torch.sum(torch.pow(avg_p, 1./self.tem), dim=1, keepdim=True)).detach()
-        loss = 0.
-        for p in ps:
-            loss += torch.mean((p-sharp_p).pow(2).sum(1))
-        loss = loss/len(ps)
-        return self.lam * loss
-
-    def computer(self, training):
+    
+    def computer(self):
         """
         propagate methods for lightGCN
         """       
         users_emb = self.embedding_user.weight
         items_emb = self.embedding_item.weight
         all_emb = torch.cat([users_emb, items_emb])
-
-        if training:
-            user_list, item_list = [], []
-            for k in range(self.sample):
-                light_out = self.rand_prop(all_emb, training=True)
-                users, items = torch.split(light_out, [self.num_users, self.num_items])
-                user_list.append(users)
-                item_list.append(items)
-            return user_list, item_list
+        #   torch.split(all_emb , [self.num_users, self.num_items])
+        embs = [all_emb]
+        if self.config['dropout']:
+            if self.training:
+                print("droping")
+                g_droped = self.__dropout(self.keep_prob)
+            else:
+                g_droped = self.Graph        
         else:
-            light_out = self.rand_prop(all_emb, training=True)
-            users, items = torch.split(light_out, [self.num_users, self.num_items])
-            return users, items
+            g_droped = self.Graph    
+        
+        for layer in range(self.n_layers):
+            if self.A_split:
+                temp_emb = []
+                for f in range(len(g_droped)):
+                    agg = torch.sparse.mm(g_droped[f], all_emb)
+                    nhid = torch.mm(agg, self.agg_weight)
+                    temp_emb.append(nhid)
+                side_emb = torch.cat(temp_emb, dim=0)
+                all_emb = side_emb
+            else:
+                all_emb = torch.sparse.mm(g_droped, all_emb)
+                all_emb = torch.mm(all_emb, self.agg_weight)
+            embs.append(all_emb)
+        embs = torch.stack(embs, dim=1)
+        #print(embs.size())
+        light_out = torch.mean(embs, dim=1)
+        users, items = torch.split(light_out, [self.num_users, self.num_items])
+        return users, items
     
-
-
     def getUsersRating(self, users):
-        # for inference
-        all_users, all_items = self.computer(False)
+        all_users, all_items = self.computer()
         users_emb = all_users[users.long()]
         items_emb = all_items
         rating = self.f(torch.matmul(users_emb, items_emb.t()))
         return rating
     
     def getEmbedding(self, users, pos_items, neg_items):
-        # for training
-        all_users_list, all_items_list = self.computer(True)
-        all_embs = []
-        for all_users, all_items in zip(all_users_list, all_items_list):
-            users_emb = all_users[users]
-            pos_emb = all_items[pos_items]
-            neg_emb = all_items[neg_items]
-            users_emb_ego = self.embedding_user(users)
-            pos_emb_ego = self.embedding_item(pos_items)
-            neg_emb_ego = self.embedding_item(neg_items)
-            all_embs.append([users_emb, pos_emb, neg_emb, users_emb_ego, pos_emb_ego, neg_emb_ego])
-        return all_embs
+        all_users, all_items = self.computer()
+        users_emb = all_users[users]
+        pos_emb = all_items[pos_items]
+        neg_emb = all_items[neg_items]
+        users_emb_ego = self.embedding_user(users)
+        pos_emb_ego = self.embedding_item(pos_items)
+        neg_emb_ego = self.embedding_item(neg_items)
+        return users_emb, pos_emb, neg_emb, users_emb_ego, pos_emb_ego, neg_emb_ego
     
     def bpr_loss(self, users, pos, neg):
-        all_embs = self.getEmbedding(users.long(), pos.long(), neg.long())
-        # (users_emb, pos_emb, neg_emb, 
-        # userEmb0,  posEmb0, negEmb0) = self.getEmbedding(users.long(), pos.long(), neg.long())
-        loss = 0.
-        pos_list, neg_list = [], []
-        for users_emb, pos_emb, neg_emb, userEmb0, posEmb0, negEmb0 in all_embs:
-            reg_loss = (1/2)*(userEmb0.norm(2).pow(2) + 
-                            posEmb0.norm(2).pow(2)  +
-                            negEmb0.norm(2).pow(2))/float(len(users))
-            pos_scores = torch.mul(users_emb, pos_emb)
-            pos_list.append(torch.log_softmax(pos_scores), dim=-1)
-            pos_scores = torch.sum(pos_scores, dim=1)
-            neg_scores = torch.mul(users_emb, neg_emb)
-            neg_list.append(torch.log_softmax(neg_scores), dim=-1)
-            neg_scores = torch.sum(neg_scores, dim=1)
-            loss += torch.mean(torch.nn.functional.softplus(neg_scores - pos_scores))
-        loss += self.consis_loss(pos_list) + self.consis_loss(neg_scores)
-
-        # #bipartite_loss = torch.mean(torch.square(userEmb0 - posEmb0)) + torch.mean(torch.square(userEmb0 - negEmb0))
-        # ue = F.softmax(userEmb0, dim=-1)
-        # pve = F.softmax(posEmb0, dim=-1)
-        # nve = F.softmax(negEmb0, dim=-1)
+        (users_emb, pos_emb, neg_emb, 
+        userEmb0,  posEmb0, negEmb0) = self.getEmbedding(users.long(), pos.long(), neg.long())
+        reg_loss = (1/2)*(userEmb0.norm(2).pow(2) + 
+                         posEmb0.norm(2).pow(2)  +
+                         negEmb0.norm(2).pow(2))/float(len(users))
+        pos_scores = torch.mul(users_emb, pos_emb)
+        pos_scores = torch.sum(pos_scores, dim=1)
+        neg_scores = torch.mul(users_emb, neg_emb)
+        neg_scores = torch.sum(neg_scores, dim=1)
+        
+        loss = torch.mean(torch.nn.functional.softplus(neg_scores - pos_scores))
+        #bipartite_loss = torch.mean(torch.square(userEmb0 - posEmb0)) + torch.mean(torch.square(userEmb0 - negEmb0))
+        ue = F.softmax(userEmb0, dim=-1)
+        pve = F.softmax(posEmb0, dim=-1)
+        nve = F.softmax(negEmb0, dim=-1)
         # bipartite_loss = torch.mean(ue * torch.log(pve)) + torch.mean(ue * torch.log(nve)) + torch.mean(pve * torch.log(ue)) + torch.mean(nve * torch.log(ue))
         # loss += self.config['ceweight'] * bipartite_loss
         
@@ -257,7 +217,7 @@ class LightGCN(BasicModel):
     def forward(self, users, items):
         # compute embedding
         all_users, all_items = self.computer()
-        print('forward!!!!!!!!!!!!!!!!!!!')
+        # print('forward')
         #all_users, all_items = self.computer()
         users_emb = all_users[users]
         items_emb = all_items[items]
